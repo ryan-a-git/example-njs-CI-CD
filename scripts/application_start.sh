49b996b1eee8a61f8c85ce7078a410977bfa19b2
unknown <r.a.anderson.96@gmail.com>
2022-04-08 14:57:39 +1000

    Fixed typo

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,10 +3,10 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+sudo chmod -R 777 /home/ec2-user/example-njs-CI-CD
 
 #navigate into our working directory where we have all our github files
-cd /home/ec2-user/express-app
+cd /home/ec2-user/example-njs-CI-CD
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"	
